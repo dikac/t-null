6ada12a22925f1994bae90db0b94e162597c37e1
dikac <rigel234@yahoo.com>
2020-09-12 00:54:45 +0700

    minor cleanup

--- spec/assert/string/null.spec.ts
+++ spec/assert/string/null.spec.ts
@@ -2,8 +2,6 @@ import Null_ from "../../../dist/assert/string/null";
 
 it("enable console log", () => { spyOn(console, 'log').and.callThrough();});
 
-let data = new Map<[boolean, any, string?], string>();
-
 it('true', ()=>{
 
     expect(Null_(true, {}, 'value')).toBe(
